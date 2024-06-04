5de14068896fe02b1630030cdae93d47b845a6e4
Adrian Roger <adrianroge@gmail.com>
2024-06-04 15:37:19 -0300

    [feat]: add sql to create database

---
+++ docs/database.sql
@@ -0,0 +1,21 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+  id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  username VARCHAR(20) UNIQUE NOT NULL,
+  first_name VARCHAR(80) NOT NULL,
+  last_name VARCHAR(80) NOT NULL,
+  email VARCHAR(80) UNIQUE NOT NULL,
+  password VARCHAR(60) NOT NULL,
+  fk_squad_id UUID,
+  is_admin BOOLEAN DEFAULT false
+);
+
+CREATE TABLE squad (
+  id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  name VARCHAR(80) UNIQUE NOT NULL,
+  fk_leader_id UUID NOT NULL REFERENCES users(id)
+);
+
+ALTER TABLE users ADD CONSTRAINT fk_squad_id FOREIGN KEY (fk_squad_id) REFERENCES squad(id);
+
