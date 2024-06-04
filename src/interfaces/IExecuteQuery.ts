14fa72ff62596e18877a9875ccee980eb23e0a4f
Adrian Roger <adrianroge@gmail.com>
2024-06-04 15:44:15 -0300

    [feat]: update interfaces folder

--- src/interfaces/IExecuteQuery.ts
+++
@@ -1,4 +0,0 @@
-export default interface IExecuteQuery {
-  query: string,
-  args?: any[]
-}
---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,21 @@
+export interface IExecuteQuery {
+  query: string,
+  args?: any[]
+}
+
+export interface IUser {
+  id?: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  squadId?: string;
+  isAdmin: boolean;
+}
+
+export interface ISquad{
+  id?:string;
+  name: string;
+  leaderId: string;
+}
